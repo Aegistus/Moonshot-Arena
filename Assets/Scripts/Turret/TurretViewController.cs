ca0451713dc159a830342e2121888429eb24d921
Aegistus <joe.ballanders@gmail.com>
2020-11-03 11:36:17 -0500

    Add Turret Swivel to Follow Player

---
+++ Assets/Scripts/Turret/TurretViewController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretViewController : MonoBehaviour
+{
+    public float swivelSpeed = .1f;
+
+    private Transform target;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    Quaternion lookRotation;
+    private void Update()
+    {
+        lookRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, swivelSpeed * Time.deltaTime);
+    }
+}

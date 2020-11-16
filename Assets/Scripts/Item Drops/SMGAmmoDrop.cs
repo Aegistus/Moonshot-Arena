056ee1811183838d80075d7fbabe8b9e7798fbb7
Aegistus <joe.ballanders@gmail.com>
2020-11-16 18:19:48 -0500

    Add Ammo and Skull Drops in Emperor's Hall

---
+++ Assets/Scripts/Item Drops/SMGAmmoDrop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMGAmmoDrop : ItemDrop
+{
+    public int ammo = 40;
+    
+    public override void GiveItem(Collider player)
+    {
+        Gun smg = (Gun)player.GetComponent<PlayerAttack>().carriedWeapons.Find(x => x.GetType() == typeof(SMG));
+        if (smg != null && smg.GetType() == typeof(SMG))
+        {
+            smg.AddAmmoToSupply(ammo);
+        }
+    }
+}

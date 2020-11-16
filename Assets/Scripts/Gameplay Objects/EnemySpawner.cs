db554e37f1de271f8a8009220298ed8d109fdc13
Aegistus <joe.ballanders@gmail.com>
2020-11-15 20:33:25 -0500

    Add Drone Spawner and Enemy Spawner Script

---
+++ Assets/Scripts/Gameplay Objects/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform spawnPoint;
+    public float respawnTime = 30f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    public IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(respawnTime);
+        }
+    }
+}

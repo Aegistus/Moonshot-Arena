e2150b59e83ba126276ef197b99f08f51e59b934
Aegistus <joe.ballanders@gmail.com>
2020-11-17 12:55:02 -0500

    Update Audio Manager to Handle Positional Audio Manually

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,14 +1,19 @@
 using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
 	public static AudioManager instance;
+	public int numOfPositionalSources = 100;
 
 	public AudioMixerGroup mixerGroup;
 
 	public Sound[] sounds;
+
+	private Queue<AudioSource> positionalSources = new Queue<AudioSource>();
 
 	void Awake()
 	{
@@ -30,6 +35,14 @@ public class AudioManager : MonoBehaviour
 
 			s.source.outputAudioMixerGroup = mixerGroup;
 		}
+		Transform positionalSourceParent = new GameObject("Positional Audio Sources").transform;
+        for (int i = 0; i < numOfPositionalSources; i++)
+        {
+			AudioSource newPositional = new GameObject().AddComponent<AudioSource>();
+			newPositional.transform.parent = positionalSourceParent;
+			newPositional.spatialBlend = 1;
+			positionalSources.Enqueue(newPositional);
+        }
 	}
 
 	public void StartPlaying(string soundName)
@@ -55,12 +68,16 @@ public class AudioManager : MonoBehaviour
 			Debug.LogWarning("Sound: " + soundName + " not found!");
 			return;
 		}
-
+		
 		sound.source.volume = sound.volume * (1f + UnityEngine.Random.Range(-sound.volumeVariance / 2f, sound.volumeVariance / 2f));
 		sound.source.pitch = sound.pitch * (1f + UnityEngine.Random.Range(-sound.pitchVariance / 2f, sound.pitchVariance / 2f));
 		sound.source.minDistance = sound.minimunDistance;
 
-		AudioSource.PlayClipAtPoint(sound.source.clip, position);
+		AudioSource source = positionalSources.Dequeue();
+		source.transform.position = position;
+		source.clip = sound.clip;
+		source.Play();
+		positionalSources.Enqueue(source);
 	}
 
 	public void StopPlaying(string soundName)

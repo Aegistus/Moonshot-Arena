8038cbe2ddc7f018e927958138cb0339ad552aaf
Aegistus <joe.ballanders@gmail.com>
2020-11-17 14:46:48 -0500

    Add Damage To Rocket Booster Trap

--- Assets/Scripts/Traps/FireTrap.cs
+++ Assets/Scripts/Traps/FireTrap.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class FireTrap : MonoBehaviour
 {
-    public List<ParticleSystem> fireFX = new List<ParticleSystem>();
+    public int damage = 10;
+    public float damageInterval = .5f;
     public float activeInterval = 5f;
     public float resetInterval = 4f;
     public float startingDelay = 0f;
 
+    private List<ParticleSystem> fireFX = new List<ParticleSystem>();
+    private List<Collider> colliders = new List<Collider>();
+    private List<Health> enteredEntities = new List<Health>();
+
     private void Start()
     {
+        colliders.AddRange(GetComponents<Collider>());
         fireFX.AddRange(GetComponentsInChildren<ParticleSystem>());
+        StartCoroutine(DoDamage());
         StartCoroutine(DelayStart());
+        foreach (var collider in colliders)
+        {
+            collider.enabled = false;
+        }
     }
 
     private IEnumerator DelayStart()
@@ -29,12 +40,52 @@ public class FireTrap : MonoBehaviour
             {
                 ps.Play();
             }
+            foreach (var collider in colliders)
+            {
+                collider.enabled = true;
+            }
             yield return new WaitForSeconds(activeInterval);
             foreach (var ps in fireFX)
             {
                 ps.Stop();
             }
+            foreach (var collider in colliders)
+            {
+                collider.enabled = false;
+            }
+            enteredEntities.Clear();
             yield return new WaitForSeconds(resetInterval);
         }
     }
+
+    private IEnumerator DoDamage()
+    {
+        while (true)
+        {
+            foreach (Health entity in enteredEntities)
+            {
+                entity.Damage(damage);
+            }
+            yield return new WaitForSeconds(damageInterval);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Health health = other.GetComponentInParent<Health>();
+        if (health && !enteredEntities.Contains(health))
+        {
+            enteredEntities.Add(health);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Health health = other.GetComponentInParent<Health>();
+        if (health)
+        {
+            enteredEntities.Remove(health);
+        }
+    }
+
 }

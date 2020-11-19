cee33f15a566fc0a95722291e0dfcf2a531732c6
Aegistus <joe.ballanders@gmail.com>
2020-11-18 20:51:30 -0500

    Add Proper Item Pickup Models

---
+++ Assets/Scripts/Item Drops/AmmoDrop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoDrop : ItemDrop
+{
+    public override void GiveItem(Collider player)
+    {
+        PlayerAttack attack = player.GetComponent<PlayerAttack>();
+        foreach (Gun weapon in attack.carriedWeapons)
+        {
+            if (weapon.GetType() == typeof(Pistol))
+            {
+                weapon.AddAmmoToSupply(40);
+            }
+            else if (weapon.GetType() == typeof(SMG))
+            {
+                weapon.AddAmmoToSupply(60);
+            }
+            else if (weapon.GetType() == typeof(Shotgun))
+            {
+                weapon.AddAmmoToSupply(24);
+            }
+        }
+    }
+}

7709c4b274ffadbc5c6a7e99c4d2ee70a7b5fa81
Aegistus <joe.ballanders@gmail.com>
2020-11-18 21:08:54 -0500

    Add Basic Tank Movement

--- Assets/Scripts/Enemies/Tank/TankController.cs
+++ Assets/Scripts/Enemies/Tank/TankController.cs
@@ -1,8 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TankController : MonoBehaviour
 {
-    
+    private NavMeshAgent navAgent;
+    private Transform target;
+
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        RetargetPlayer(GameObject.FindGameObjectWithTag("Player"));
+        StartCoroutine(HuntPlayer());
+        RespawnManager.OnPlayerRespawn += RetargetPlayer;
+    }
+
+    private void RetargetPlayer(GameObject newPlayer)
+    {
+        target = newPlayer.transform;
+        StartCoroutine(HuntPlayer());
+    }
+
+    private IEnumerator HuntPlayer()
+    {
+        while (target != null)
+        {
+            navAgent.SetDestination(target.position);
+            yield return new WaitForSeconds(1);
+        }
+    }
 }

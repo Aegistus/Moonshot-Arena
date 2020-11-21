c823fb59dae2cc70e1a02cfca7951e2713f731a8
Aegistus <joe.ballanders@gmail.com>
2020-11-21 18:54:44 -0500

    Adjust Sliding Height and Mechanics

--- Assets/Scripts/Player/States/Sliding.cs
+++ Assets/Scripts/Player/States/Sliding.cs
@@ -6,7 +6,8 @@ public class Sliding : PlayerState
 {
     private float initialBoost = 5f;
     private Transform camTransform;
-    private float slideDuck = .5f;
+    private float slideDuck = .25f;
+    private CapsuleCollider standingCollider;
 
     public Sliding(GameObject gameObject) : base(gameObject)
     {
@@ -15,19 +16,23 @@ public class Sliding : PlayerState
         transitionsTo.Add(new Transition(typeof(Idling), Not(MoveKeys), Not(Ctrl)));
         transitionsTo.Add(new Transition(typeof(Jumping), Spacebar, Not(Falling)));
         transitionsTo.Add(new Transition(typeof(Falling), Not(OnGround), Falling));
-        camTransform = Camera.main.transform;
+        //camTransform = Camera.main.transform;
+        standingCollider = gameObject.GetComponent<CapsuleCollider>();
     }
 
     public override void AfterExecution()
     {
-        camTransform.localPosition = Vector3.zero;
+        //transform.localPosition = Vector3.zero;
+        standingCollider.direction = 1;
     }
 
     public override void BeforeExecution()
     {
         Debug.Log("Sliding");
-        camTransform.Translate(0, -slideDuck, 0, Space.Self);
+        standingCollider.direction = 2;
+        transform.Translate(0, -slideDuck, 0, Space.Self);
         movement.AddVelocity(transform.forward * initialBoost);
+        movement.AddVelocity(-transform.up);
         AudioManager.instance.StartPlaying("Slide");
     }
 

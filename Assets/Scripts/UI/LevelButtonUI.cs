45e197e2dd22ae6f36821bc9d66a51bbb06f4660
Aegistus <joe.ballanders@gmail.com>
2020-11-22 12:58:15 -0500

    Add Level Selection Menu

---
+++ Assets/Scripts/UI/LevelButtonUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelButtonUI : MonoBehaviour
+{
+    public string connectedLevel;
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(connectedLevel);
+    }
+}

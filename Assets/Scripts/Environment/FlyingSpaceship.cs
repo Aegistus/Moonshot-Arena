e07e2e93890b1abd91372c7885f875d856e41184
Aegistus <joe.ballanders@gmail.com>
2020-11-27 12:32:24 -0500

    Add Background Flying Spaceships in Hangar Level

---
+++ Assets/Scripts/Environment/FlyingSpaceship.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingSpaceship : MonoBehaviour
+{
+    public float shipSpeed = 5f;
+    public float resetTime = 20f;
+
+    private Vector3 startingPosition;
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+        StartCoroutine(ResetPosition());
+    }
+
+    private void Update()
+    {
+        transform.Translate(0, 0, shipSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator ResetPosition()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(resetTime);
+            transform.position = startingPosition;
+        }
+    }
+}
